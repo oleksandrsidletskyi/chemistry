c184558c149db39ee44860501e9e0ada5b9c7ff3
oleksandrsidletskyi <sidleckiy.av@gmail.com>
2022-10-22 14:18:18 +0300

    Commit 12

--- chemistry/urls.py
+++ chemistry/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
+from django.conf.urls.static import static
 from . import settings
 
 
@@ -9,4 +10,4 @@ urlpatterns = [
     path('', include('main.urls')),
     path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

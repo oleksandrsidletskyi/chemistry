79235a7bc7f4275d26d2b11a8b0f986929345bf1
oleksandrsidletskyi <sidleckiy.av@gmail.com>
2022-10-22 16:24:21 +0300

    Commit 13

--- chemistry/settings.py
+++ chemistry/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "../main/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "../chemistry/main/static")
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]

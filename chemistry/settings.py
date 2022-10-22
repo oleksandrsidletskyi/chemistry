e78bab529f4a748e41bbe6bf5893187831ed690c
oleksandrsidletskyi <sidleckiy.av@gmail.com>
2022-10-22 16:54:47 +0300

    Commit 14

--- chemistry/settings.py
+++ chemistry/settings.py
@@ -123,6 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "../chemistry/main/static")
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")

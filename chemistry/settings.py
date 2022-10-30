f9d1d5a17b8f0734b89ee31dfc47e24e1d90fd2d
oleksandrsidletskyi <sidleckiy.av@gmail.com>
2022-10-30 18:48:17 +0200

    Commit 24

--- chemistry/settings.py
+++ chemistry/settings.py
@@ -120,10 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "../chemistry/main/static")
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = 'media/'
